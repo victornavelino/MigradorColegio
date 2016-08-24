1dec529231ec6bdc0a4d51a9c6117ba51ad2bce7
hugo <hugo@debianHugo>
2016-08-24 18:28:12 -0300

    add medotods

--- src/migradorcolegiomedico/ImportarExcelMedicos.java
+++ src/migradorcolegiomedico/ImportarExcelMedicos.java
@@ -6,14 +6,18 @@
 package migradorcolegiomedico;
 
 import Entidades.Medico.Medico;
+import Entidades.Persona.Persona;
 import Facades.MedicoFacade;
 import java.io.File;
-import jxl.*; 
+import jxl.*;
+
 /**
  *
  * @author hugo
  */
 public class ImportarExcelMedicos {
+
+    private Medico medico;
 
     private void leerArchivoExcel(String archivoDestino) {
 
@@ -22,22 +26,42 @@ public class ImportarExcelMedicos {
             System.out.println("Número de Hojas\t"
                     + archivoExcel.getNumberOfSheets());
 
-                Sheet hoja = archivoExcel.getSheet(0);
-                int numColumnas = hoja.getColumns();
-                int numFilas = hoja.getRows();
-                String data;
-                System.out.println("Nombre de la Hoja\t"
-                        + archivoExcel.getSheet(0).getName());
-                for (int fila = 0; fila < numFilas; fila++) { // Recorre cada fila de la  hoja 
-                    for (int columna = 0; columna < numColumnas; columna++) { // Recorre  cada                                     
+            Sheet hoja = archivoExcel.getSheet(0);
+            int numColumnas = hoja.getColumns();
+            int numFilas = hoja.getRows();
+            String dato;
+            System.out.println("Nombre de la Hoja\t"
+                    + archivoExcel.getSheet(0).getName());
+            // Recorre cada fila de la  hoja 
+            for (int fila = 0; fila < numFilas; fila++) {
+                medico = new Medico();
+                medico.setPersona(new Persona());
+
+                for (int columna = 0; columna < numColumnas; columna++) { // Recorre  cada                                     
 // fila 
-                        data = hoja.getCell(columna, fila).getContents();
-                        System.out.print(data + " ");
+                    dato = hoja.getCell(columna, fila).getContents();
+                    switch (String.valueOf(columna)) {
+                        case "0":
+                            medico.setMatriculaProfesional(dato);
+                            break;
+                        case "1":
+                            try {
+                                String apellidoNombreArray[];
+                                apellidoNombreArray = dato.split("\\,");
+                                medico.getPersona().setApellido(apellidoNombreArray[0]);
+                                medico.getPersona().setNombre(apellidoNombreArray[1].trim());
+                            } catch (Exception e) {
+                            }
+                            break;
 
                     }
-                    System.out.println("\n");
+                    System.out.print(dato + " ");
+
                 }
-            
+                MedicoFacade.getInstance().alta(medico);
+                System.out.println("\n");
+            }
+
         } catch (Exception ioe) {
             ioe.printStackTrace();
         }
@@ -51,9 +75,9 @@ public class ImportarExcelMedicos {
     }
 
     private void cargar() {
-       Medico medico=new Medico();
-       medico.setMatriculaProfesional("2044");
+        Medico medico = new Medico();
+        medico.setMatriculaProfesional("2044");
         MedicoFacade.getInstance().alta(medico);
     }
-    
+
 }
