dff562d5f100f561ce43a868a4a16d28e65fee37
franco <franco@franco-notebook>
2016-09-06 00:06:53 -0300

    Primera versión migrador

---
+++ src/Controladores/EstadoCivilJpaController.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controladores;
+
+import Controladores.exceptions.NonexistentEntityException;
+import Entidades.Persona.EstadoCivil;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+/**
+ *
+ * @author franco
+ */
+public class EstadoCivilJpaController implements Serializable {
+
+    public EstadoCivilJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(EstadoCivil estadoCivil) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(estadoCivil);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(EstadoCivil estadoCivil) throws NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            estadoCivil = em.merge(estadoCivil);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Long id = estadoCivil.getId();
+                if (findEstadoCivil(id) == null) {
+                    throw new NonexistentEntityException("The estadoCivil with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Long id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            EstadoCivil estadoCivil;
+            try {
+                estadoCivil = em.getReference(EstadoCivil.class, id);
+                estadoCivil.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The estadoCivil with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(estadoCivil);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<EstadoCivil> findEstadoCivilEntities() {
+        return findEstadoCivilEntities(true, -1, -1);
+    }
+
+    public List<EstadoCivil> findEstadoCivilEntities(int maxResults, int firstResult) {
+        return findEstadoCivilEntities(false, maxResults, firstResult);
+    }
+
+    private List<EstadoCivil> findEstadoCivilEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(EstadoCivil.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public EstadoCivil findEstadoCivil(Long id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(EstadoCivil.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getEstadoCivilCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<EstadoCivil> rt = cq.from(EstadoCivil.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
--- src/migradorcolegiomedico/ImportarExcelMedicos.java
+++ src/migradorcolegiomedico/ImportarExcelMedicos.java
@@ -5,16 +5,31 @@
  */
 package migradorcolegiomedico;
 
+import Controladores.DocumentoIdentidadJpaController;
+import Controladores.EstadoCivilJpaController;
 import Controladores.SexoJpaController;
+import Controladores.TipoDocumentoJpaController;
 import Entidades.Medico.Medico;
+import Entidades.Persona.DocumentoIdentidad;
+import Entidades.Persona.Domicilio;
+import Entidades.Persona.EstadoCivil;
 import Entidades.Persona.Persona;
+import Entidades.Persona.Telefono;
 import Facades.MedicoFacade;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import jxl.*;
+import jxl.read.biff.BiffException;
 
 /**
  *
@@ -26,80 +41,167 @@ public class ImportarExcelMedicos {
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("MigradorColegioMedicoPU");
     SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd");
 
-    private void leerArchivoExcel(String archivoDestino) {
-
-        try {
-            Workbook archivoExcel = Workbook.getWorkbook(new File(archivoDestino));
-            System.out.println("Número de Hojas\t"
-                    + archivoExcel.getNumberOfSheets());
-
-            Sheet hoja = archivoExcel.getSheet(0);
-            int numColumnas = hoja.getColumns();
-            int numFilas = hoja.getRows();
-            String dato;
-            System.out.println("Nombre de la Hoja\t"
-                    + archivoExcel.getSheet(0).getName());
-            // Recorre cada fila de la  hoja 
-            for (int fila = 1; fila < numFilas; fila++) {
-                medico = new Medico();
-                medico.setPersona(new Persona());
-
-                for (int columna = 0; columna < numColumnas; columna++) { // Recorre  cada                                     
-// fila 
-                    dato = hoja.getCell(columna, fila).getContents();
-                    switch (String.valueOf(columna)) {
-                        case "0":
-                            medico.setMatriculaProfesional(dato);
-                            break;
-                        case "1":
-                            try {
-                                String apellidoNombreArray[];
-                                apellidoNombreArray = dato.split("\\,");
-                                medico.getPersona().setApellido(apellidoNombreArray[0]);
-                                medico.getPersona().setNombre(apellidoNombreArray[1].trim());
-                            } catch (Exception e) {
-                            }
-                            break;
-                        case "2":
-                            if (dato.contains("Masculino")) {
-                                medico.getPersona().setSexo(new SexoJpaController(emf).findSexo(1L));
-                            }
-                            if (dato.contains("Femenino")) {
-                                medico.getPersona().setSexo(new SexoJpaController(emf).findSexo(2L));
-                            }
-                            break;
-                        case "3":
-                            try {
-                                medico.getPersona().setFechaNacimiento(formatoFecha.parse(dato.substring(0, 10)));
-                            } catch (Exception e) {
-                            }
-                            break;
-                        case "4":
-                            medico.getPersona().setEstadoCivil(null);
-
-                    }
-                    System.out.print(dato + " ");
-
-                }
-                MedicoFacade.getInstance().alta(medico);
-                System.out.println("\n");
-            }
-
-        } catch (Exception ioe) {
-            ioe.printStackTrace();
-        }
-
-    }
-
     public static void main(String arg[]) {
+        System.out.println("Importador de Archivos:");
         ImportarExcelMedicos excel = new ImportarExcelMedicos();
-        excel.leerArchivoExcel("/home/nago/LEGAJOMEDICO.xls");
+        excel.importar();//LEGAJOMEDICO.xls --> MEDICOS
     }
 
-    private void cargar() {
-        Medico medico = new Medico();
-        medico.setMatriculaProfesional("2044");
-        MedicoFacade.getInstance().alta(medico);
+    private void importar() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+        int result = fileChooser.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            try {
+                File selectedFile = fileChooser.getSelectedFile();
+                System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+                boolean flag = true;
+                InputStream in = null;
+                in = new FileInputStream(selectedFile);
+
+                Workbook workbook = Workbook.getWorkbook(in);
+                Sheet sheet = workbook.getSheet(0);
+                String dato;
+                // Recorre cada fila de la  hoja 
+                for (int fila = 1; fila < sheet.getRows(); fila++) {
+                    medico = new Medico();
+                    medico.setPersona(new Persona());
+
+                    for (int columna = 0; columna < sheet.getColumns(); columna++) { // Recorre  cada fila 
+                        dato = sheet.getCell(columna, fila).getContents();
+                        switch (String.valueOf(columna)) {
+                            case "0":
+                                medico.setMatriculaProfesional(dato);
+                                break;
+                            case "1":
+                                try {
+                                    String apellidoNombreArray[];
+                                    apellidoNombreArray = dato.split("\\,");
+                                    medico.getPersona().setApellido(apellidoNombreArray[0]);
+                                    medico.getPersona().setNombre(apellidoNombreArray[1].trim());
+                                } catch (Exception e) {
+                                }
+                                break;
+                            case "2":
+                                try {
+                                    if (dato.contains("Masculino")) {
+                                        medico.getPersona().setSexo(new SexoJpaController(emf).findSexo(1L));
+                                    } else if (dato.contains("Femenino")) {
+                                        medico.getPersona().setSexo(new SexoJpaController(emf).findSexo(2L));
+                                    }
+                                } catch (Exception e) {
+                                }
+                                break;
+                            case "3":
+                                try {
+                                    medico.getPersona().setFechaNacimiento(formatoFecha.parse(dato.substring(0, 10)));
+                                } catch (Exception e) {
+                                }
+                                break;
+                            case "4":
+                                try {
+                                    EstadoCivil civilBuscado = new EstadoCivilJpaController(emf).findEstadoCivil(Long.parseLong(dato));
+                                    if (civilBuscado == null) {
+                                        civilBuscado = new EstadoCivil();
+                                        civilBuscado.setId(Long.parseLong(dato));
+                                        civilBuscado.setDescripcion(dato);
+                                        new EstadoCivilJpaController(emf).create(civilBuscado);
+                                    }
+                                    medico.getPersona().setEstadoCivil(civilBuscado);
+                                } catch (NumberFormatException numberFormatException) {
+                                }
+                                break;
+                            case "5":
+                                medico.getPersona()
+                                        .setDocumentoIdentidad(
+                                                new DocumentoIdentidad());
+                                try {
+                                    if (dato.contains("DNI")) {
+                                        medico.getPersona().getDocumentoIdentidad().setTipoDocumento(
+                                                new TipoDocumentoJpaController(emf).findTipoDocumento(1L));
+                                    }
+                                } catch (Exception e) {
+                                }
+                                break;
+                            case "6":
+                                try {
+                                    medico.getPersona().getDocumentoIdentidad().setNumero(Long.parseLong(dato));
+                                } catch (NumberFormatException numberFormatException) {
+                                }
+                                break;
+                            case "7":
+                                //Nacionalidad
+                                break;
+                            case "8":
+                                //Provincia
+                                medico.getPersona().setDomicilio(new Domicilio());
+                                // medico.getPersona().getDomicilio().setLocalidad(null);
+                                break;
+                            case "9":
+                                //Departamento
+                                // medico.getPersona().getDomicilio().setLocalidad(null);
+                                break;
+
+                            case "10":
+                                //Localidad
+                                // medico.getPersona().getDomicilio().setLocalidad(null);
+                                break;
+
+                            case "11":
+                                //barrio
+                                medico.getPersona().getDomicilio().setBarrio(dato);
+                                break;
+                            case "12":
+                                //Calle
+                                medico.getPersona().getDomicilio().setCalle(dato);
+                                break;
+                            case "13":
+                                //numero
+                                medico.getPersona().getDomicilio().setNumero(dato);
+                                break;
+                            case "14":
+                                //piso
+                                medico.getPersona().getDomicilio().setPiso(dato);
+                                break;
+                            case "15":
+                                //dpto
+                                medico.getPersona().getDomicilio().setDpto(dato);
+                                break;
+                            case "16":
+                                //codigio postal
+                                medico.getPersona().getDomicilio().setCodigoPostal(dato);
+                                break;
+                            case "17":
+                                //Telefono
+                                List<Telefono> telefonos = new ArrayList<>();
+                                telefonos.add(new Telefono());
+                                telefonos.get(0).setNumero(dato);
+                                medico.getPersona().setTelefonos(telefonos);//falta agregar el tipo
+                                break;
+                            case "18":
+                                //Celular
+                                Telefono t = new Telefono();
+                                t.setNumero(dato);
+                                medico.getPersona().getTelefonos().add(t);
+                                ;//falta agregar el tipo
+
+                                break;
+                        }
+                    }
+                    MedicoFacade.getInstance().alta(medico);
+                }
+            } catch (FileNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error:" + ex);
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(null, "Error:" + ex);
+            } catch (BiffException ex) {
+                JOptionPane.showMessageDialog(null, "Error:" + ex);
+            }
+        }
+    }
+
+    public String quitarComas(String entrada) {
+        return entrada.replace("" + "$" + "", "").replace(".", "").replace(",", ".").replace('"', ' ').trim();
     }
 
 }
